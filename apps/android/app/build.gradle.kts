2e7047afc2abcf6018c24801dbf6cefe27c8c0e9
zcx369658780 <zcxvegeta@126.com>
2026-03-19 17:45:24 +0800

    fix(android): use ip/http in debug and domain/https in release

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -25,8 +25,14 @@ android {
     }
 
     buildTypes {
+        debug {
+            buildConfigField("String", "API_BASE_URL", "\"http://101.133.161.203/\"")
+            buildConfigField("String", "WS_BASE_URL", "\"ws://101.133.161.203:8081/\"")
+        }
         release {
             isMinifyEnabled = false
+            buildConfigField("String", "API_BASE_URL", "\"https://slowdate.top/\"")
+            buildConfigField("String", "WS_BASE_URL", "\"wss://slowdate.top/\"")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
