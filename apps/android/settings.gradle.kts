f77a2d59e6531dc73fda8bdb58f407cd8f1878d5
zcx369658780 <zcxvegeta@126.com>
2026-03-27 17:08:22 +0800

    ci: force flutter module deps to resolve from local aar repo

--- apps/android/settings.gradle.kts
+++ apps/android/settings.gradle.kts
@@ -10,9 +10,14 @@ dependencyResolutionManagement {
     // Flutter module Gradle plugin adds transient repositories during module integration.
     repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
+        maven(url = uri("../flutter_elitesync_module/build/host/outputs/repo")) {
+            content {
+                includeGroup("com.elitesync.flutter_elitesync_module")
+                includeGroup("io.flutter")
+            }
+        }
         google()
         mavenCentral()
-        maven(url = uri("../flutter_elitesync_module/build/host/outputs/repo"))
         maven(url = uri("https://storage.googleapis.com/download.flutter.io"))
     }
 }
