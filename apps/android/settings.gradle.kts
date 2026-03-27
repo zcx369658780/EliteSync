66d26406b84768846b155d2d5350c6d569a1b986
zcx369658780 <zcxvegeta@126.com>
2026-03-27 17:35:57 +0800

    fix(ci): remove restrictive local repo content filter for flutter plugins

--- apps/android/settings.gradle.kts
+++ apps/android/settings.gradle.kts
@@ -10,12 +10,7 @@ dependencyResolutionManagement {
     // Flutter module Gradle plugin adds transient repositories during module integration.
     repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
-        maven(url = uri("../flutter_elitesync_module/build/host/outputs/repo")) {
-            content {
-                includeGroup("com.elitesync.flutter_elitesync_module")
-                includeGroup("io.flutter")
-            }
-        }
+        maven(url = uri("../flutter_elitesync_module/build/host/outputs/repo"))
         google()
         mavenCentral()
         maven(url = uri("https://storage.googleapis.com/download.flutter.io"))
